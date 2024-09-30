47477bb92ceff320562cb4300c07d279a604021a
Hannarong <114913567+saran1185@users.noreply.github.com>
2024-09-30 14:05:13 +0700

    initial commit

---
+++ src/main/java/org/example/AdvancedCalculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class AdvancedCalculator extends Calculator {
+    public double power(int base, int exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public double sqrt(int a) throws IllegalArgumentException {
+        if (a < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number.");
+        }
+        return Math.sqrt(a);
+    }
+}
---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+}
